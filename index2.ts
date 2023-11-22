85d2f18fe333fc23c6ddd9c0c1940b6be16973a5
Jignesh <jignesh.sharma.707@gmail.com>
2023-11-23 03:27:48 +0530

    arrays

---
+++ index2.ts
@@ -0,0 +1,19 @@
+/////////////// Arrays in ts  /////////////////
+// []notation
+// ---
+const nums: number[] = [1, 2, 3, 4, 4, 5]
+// const nums1: number[] = [1, 2, 3, 4, 4, "give"]
+// console.log(nums);
+
+const str: string[] = ["one", "two"]
+// const str2: string[] = ["one", 1]
+// console.log(str);
+
+// Array<type>notation
+//---
+const item: Array<string> = ["jinu"]
+// const item1: Array<string> = [1, "jinu"]
+// console.log(item);
+
+
+/////////////// Multi dimensional  /////////////////
