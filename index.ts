2168c1e56bb04ac88ec1ff09a28c0751d9a14952
Jignesh <jignesh.sharma.707@gmail.com>
2023-11-23 02:58:11 +0530

    typecript

---
+++ index.ts
@@ -0,0 +1,62 @@
+
+/////////////Annotation//////////////
+
+// Strings
+let myName: string = "jinu"
+myName = "jinu sharma"
+// console.log(myName);
+
+
+// Numbers
+let favNumber: number = 1
+// console.log(favNumber);
+
+// Boolean
+let areReady: boolean = true
+// console.log(areReady);
+
+
+/////////// Inferring ////////////////////
+let tech = "Java"
+// tech = 3
+let tech1 = 1
+// tech1 = "jo"
+
+let tech3 = true
+// tech3 = ""
+
+// console.log(typeof tech);
+// console.log(typeof tech1);
+// console.log(typeof tech3);
+
+
+/////////////// Any Type /////////////////
+
+let color: any
+color = "blue"
+color = 3
+
+
+
+// let color1: string = "blue"
+// color1 = 3
+// console.log(color);
+
+
+/////////////// Function parameters and annotations /////////////////
+
+function add(num: number) {
+    return num + 2
+}
+// console.log(add(3));
+
+
+const addTwoNumber = (x: number, y: number) => {
+    return x + y
+
+}
+// console.log(addTwoNumber(1, 1));
+
+
+
+
