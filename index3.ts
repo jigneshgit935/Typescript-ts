d70ed70faaeeb6f36775eac52ca6eff4489895c6
Jignesh <jignesh.sharma.707@gmail.com>
2023-11-23 06:22:13 +0530

    Intersection types

--- index3.ts
+++ index3.ts
@@ -1,11 +1,11 @@
 /////////////// Optional Properties  /////////////////
 type User = {
     name: string
-    age: number
+    readonly age: number
     email?: string
 }
 
-const person: User = {
+const personhere: User = {
     name: "Jignesh",
     age: 31,
     email: "jignesh@gmail.com"
@@ -16,6 +16,33 @@ const person2: User = {
     age: 31,
     email: "jignesh@gmail.com"
 }
-console.log(`Name: ${person.name}, Age: ${person.age}, Email: ${person.email} `);
 
-console.log(`Name: ${person2.name}, Age: ${person2.age}`);
+// console.log(`Name: ${personhere.name}, Age: ${personhere.age}, Email: ${personhere.email} `);
+
+// person2.age = 23
+// console.log(`Name: ${person2.name}, Age: ${person2.age}`);
+
+
+/////////////// Intersection types  /////////////////
+type UserInfo = {
+    name: string
+    age: number
+}
+
+type UserInfo2 = {
+    email: string
+    password: string
+}
+
+
+type UserInfoAndUserInfo2 = UserInfo & UserInfo2
+
+const alice: UserInfoAndUserInfo2 = {
+    name: "jinu",
+    age: 32,
+    email: "jinu@gmail.com",
+    password: '12345'
+
+}
+
+// console.log(alice.age, alice.email, alice.name, alice.password);
