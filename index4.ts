5ee3b2b396bb78096cb6bb7d0f43cbd7245ca41e
Jignesh <jignesh.sharma.707@gmail.com>
2023-11-23 07:10:17 +0530

    Access Modifiers

--- index4.ts
+++ index4.ts
@@ -16,3 +16,37 @@ const jinu = new Jinu("Jignesh", 22)
 
 
 /////////////// Access Modifiers  /////////////////
+// there are three access modifiers = public private protected
+
+class Persona {
+    private first: string
+    // public first: string
+    public last: string
+    protected age: number
+    constructor(first: string, last: string) {
+        this.first = first
+        this.last = last
+    }
+
+    getName(): string {
+        return `${this.first} ${this.last} ${this.age}`
+    }
+}
+
+
+let p1 = new Persona("jinu", "sharma")
+console.log(p1);
+console.log(p1.getName());
+console.log(p1.last);
+
+
+
+class Human extends Persona {
+    constructor(first: string, last: string, age: number) {
+        super(first, last)
+        this.age = age
+    }
+}
+
+const jin = new Human("jignesh", "sharma", 23)
+console.log(jin);
