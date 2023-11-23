fda1b24dc3e99fe03fe8aa0f91e1821cefc9f9ed
Jignesh <jignesh.sharma.707@gmail.com>
2023-11-23 06:55:16 +0530

    class annotations

---
+++ index4.ts
@@ -0,0 +1,18 @@
+/////////////// Class Properties Annotations  /////////////////
+class Jinu {
+    name: string
+    age: number
+
+    constructor(name: string, age: number) {
+        this.name = "jinu"
+        this.age = 23
+    }
+}
+
+const jinu = new Jinu("Jignesh", 22)
+// console.log(jinu);
+
+
+
+
+/////////////// Access Modifiers  /////////////////
