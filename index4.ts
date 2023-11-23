56444d729923a8a4a6975b20f0c47b73a9a61421
Jignesh <jignesh.sharma.707@gmail.com>
2023-11-23 15:07:41 +0530

    Getters and Setters

--- index4.ts
+++ index4.ts
@@ -55,4 +55,27 @@ const jin = new Human("jignesh", "sharma", 23)
 
 
 
-///////////////   /////////////////
+/////////////// Getters and Setters  /////////////////
+class myClass {
+    private _myProperty: number = 0
+
+
+    get myProperty(): number {
+        return this._myProperty
+    }
+
+    set myProperty(value: number) {
+        this._myProperty = value
+    }
+
+}
+
+
+const myInstance = new myClass()
+console.log(`Current Value ${myInstance.myProperty}`);
+
+myInstance.myProperty = 2
+console.log(`Current Value ${myInstance.myProperty}`);
+
+
+
